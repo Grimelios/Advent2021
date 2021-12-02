f2cc199d1700f028fea34d7b8c4e5961b431c230
Mark Hatfield <grimelios@gmail.com>
2021-12-02 09:56:34 -0600

    Day 2

--- Advent2021/Solvers/Day2.cs
+++ Advent2021/Solvers/Day2.cs
@@ -10,12 +10,52 @@ namespace Advent2021.Solvers
 	{
 		protected override object Part1(string[] lines)
 		{
-			throw new NotImplementedException();
+			var x = 0;
+			var y = 0;
+
+			foreach (var s in lines)
+			{
+				var tokens = s.Split(' ');
+				var value = int.Parse(tokens[1]);
+
+				switch (tokens[0])
+				{
+					case "forward": x += value; break;
+					case "up": y -= value; break;
+					case "down": y += value; break;
+				}
+			}
+
+			return x * y;
 		}
 
 		protected override object Part2(string[] lines)
 		{
-			throw new NotImplementedException();
+			var x = 0;
+			var aim = 0;
+			var depth = 0;
+
+			foreach (var s in lines)
+			{
+				var tokens = s.Split(' ');
+				var value = int.Parse(tokens[1]);
+
+				switch (tokens[0])
+				{
+					case "forward":
+					{
+						x += value;
+						depth += aim * value;
+
+						break;
+					}
+
+					case "up": aim -= value; break;
+					case "down": aim += value; break;
+				}
+			}
+
+			return x * depth;
 		}
 	}
 }
